b749dd5ed1f64a0f701b7b565e9ed066fca7f259
AMD <luisleo526@yahoo.com>
2021-01-27 19:48:24 +0800

    Wave

--- Wave/src/ns_solver/ns_ppe_mg_solver.f90
+++ Wave/src/ns_solver/ns_ppe_mg_solver.f90
@@ -2,7 +2,7 @@ subroutine ppe_mg_solver(input)
 use all
 !$ use omp_lib
 implicit none
-integer :: iter, id
+integer :: iter, id, initer
 integer :: i,j,k,input
 integer(8) :: cpustart, cpuend
 real(8) :: grow
@@ -12,7 +12,9 @@ call system_clock(cpustart)
 call ppe_mg_solver_init
 call ppe_mg_solver_src(input)
 
-do iter = 1, 100
+initer=0
+
+110 do iter = 1, 30
 
     p%glb%piter=p%glb%piter+1
 
@@ -26,50 +28,30 @@ do iter = 1, 100
 
     grow = p%of(0)%loc%mg(1)%l2norm / p%of(0)%loc%mg(1)%l2norm0
     
-    if( grow > 1.0d0 )then
-        if( p%of(0)%loc%mg(1)%l2norm < p%glb%p_tol )then
-            goto 115
+    if( grow > 1.0d0 .or. abs(grow-1.0d0)<0.01 )then
+        if( initer < 5 )then
+            initer=initer+1
+            call ppe_mg_get_pressure
+            call ppe_mg_solver_src(0,1.5d0)
+            goto 110
+        else
+            if( p%of(0)%loc%mg(1)%l2norm < p%glb%p_tol )then
+                goto 115
+            endif
         endif
     else if ( p%of(0)%loc%mg(1)%l2norm < p%glb%p_tol*0.001d0 ) then
         goto 115
     endif
    
-    if(mod(iter,10).eq.0)then
-        write(*,*)"Final:",iter,p%of(0)%loc%mg(1)%l2norm
-    endif
+    !if(mod(iter,50).eq.0)then
+    !    write(*,*)"Final:",initer,iter,p%of(0)%loc%mg(1)%l2norm
+    !endif
     
 enddo
 
-if( p%of(0)%loc%mg(1)%l2norm < p%glb%p_tol )then
-    goto 115
-else
-    call ppe_sor_solver(1.0d-8)
-    return
-endif
-
-
 115 p%glb%ppe_linf = p%of(0)%loc%mg(1)%l2norm
 
-!$omp parallel do private(i,j,k)
-do id = 0, p%glb%threads-1
-    
-    do k = p%of(id)%loc%ks, p%of(id)%loc%ke
-    do j = p%of(id)%loc%js, p%of(id)%loc%je
-    do i = p%of(id)%loc%is, p%of(id)%loc%ie 
-        p%of(id)%loc%p%now(i,j,k) = p%of(id)%loc%mg(1)%sol(i-p%of(id)%loc%is+1,&
-                                                        &  j-p%of(id)%loc%js+1,&
-                                                        &  k-p%of(id)%loc%ks+1)
-                                                            
-    enddo
-    enddo
-    enddo
-    
-    call p%of(id)%bc(0,p%of(id)%loc%p%now)
-
-enddo    
-!$omp end parallel do
-
-call pt%p%sync
+call ppe_mg_get_pressure
 call ppe_mg_correction
 
 call system_clock(cpuend)
@@ -153,43 +135,63 @@ integer :: id,i,j,k
     
 end subroutine
 
-subroutine ppe_mg_solver_src(iter)
+subroutine ppe_mg_solver_src(iter,w)
 use all
 !$ use omp_lib
 implicit none
 integer :: id,i,j,k,iter
 real(8) :: pi,x,y
+real(8), optional :: w
 
-if(iter>3)then
+if( present(w) )then
+
     !$omp parallel do private(i,j,k)
     do id = 0, p%glb%threads-1
         
         do k = p%of(id)%loc%ks-p%of(id)%glb%ghc, p%of(id)%loc%ke+p%of(id)%glb%ghc
         do j = p%of(id)%loc%js-p%of(id)%glb%ghc, p%of(id)%loc%je+p%of(id)%glb%ghc
         do i = p%of(id)%loc%is-p%of(id)%glb%ghc, p%of(id)%loc%ie+p%of(id)%glb%ghc   
-            p%of(id)%loc%p%tmp(i,j,k) = 2.0d0*p%of(id)%loc%p%old(i,j,k) - p%of(id)%loc%p%old2(i,j,k)        
+            p%of(id)%loc%p%tmp(i,j,k) = w*p%of(id)%loc%p%now(i,j,k) + (1.0d0-w)*p%of(id)%loc%p%tmp(i,j,k)
         enddo
         enddo
         enddo
         
     enddo
     !$omp end parallel do
+
 else
-    !$omp parallel do private(i,j,k)
-    do id = 0, p%glb%threads-1
-        
-        do k = p%of(id)%loc%ks-p%of(id)%glb%ghc, p%of(id)%loc%ke+p%of(id)%glb%ghc
-        do j = p%of(id)%loc%js-p%of(id)%glb%ghc, p%of(id)%loc%je+p%of(id)%glb%ghc
-        do i = p%of(id)%loc%is-p%of(id)%glb%ghc, p%of(id)%loc%ie+p%of(id)%glb%ghc   
-            p%of(id)%loc%p%tmp(i,j,k) = p%of(id)%loc%p%now(i,j,k) 
-        enddo
-        enddo
-        enddo
-        
-    enddo
-    !$omp end parallel do
-endif
 
+    if(iter>3)then
+        !$omp parallel do private(i,j,k)
+        do id = 0, p%glb%threads-1
+            
+            do k = p%of(id)%loc%ks-p%of(id)%glb%ghc, p%of(id)%loc%ke+p%of(id)%glb%ghc
+            do j = p%of(id)%loc%js-p%of(id)%glb%ghc, p%of(id)%loc%je+p%of(id)%glb%ghc
+            do i = p%of(id)%loc%is-p%of(id)%glb%ghc, p%of(id)%loc%ie+p%of(id)%glb%ghc   
+                p%of(id)%loc%p%tmp(i,j,k) = 2.0d0*p%of(id)%loc%p%old(i,j,k) - p%of(id)%loc%p%old2(i,j,k)        
+            enddo
+            enddo
+            enddo
+            
+        enddo
+        !$omp end parallel do
+    else
+        !$omp parallel do private(i,j,k)
+        do id = 0, p%glb%threads-1
+            
+            do k = p%of(id)%loc%ks-p%of(id)%glb%ghc, p%of(id)%loc%ke+p%of(id)%glb%ghc
+            do j = p%of(id)%loc%js-p%of(id)%glb%ghc, p%of(id)%loc%je+p%of(id)%glb%ghc
+            do i = p%of(id)%loc%is-p%of(id)%glb%ghc, p%of(id)%loc%ie+p%of(id)%glb%ghc   
+                p%of(id)%loc%p%tmp(i,j,k) = p%of(id)%loc%p%now(i,j,k) 
+            enddo
+            enddo
+            enddo
+            
+        enddo
+        !$omp end parallel do
+    endif
+
+endif
 
 !$omp parallel do private(i,j,k)
 do id = 0, p%glb%threads-1
@@ -226,7 +228,7 @@ do id = 0, p%glb%threads-1
 enddo    
 !$omp end parallel do
 
-    call pt%mg%sync(1)
+call pt%mg%sync(1)
 
 end subroutine
 
@@ -271,4 +273,33 @@ real(8) :: src
     
     call ns_velbc
     
+end subroutine
+
+subroutine ppe_mg_get_pressure()
+use all 
+!$use omp_lib
+implicit none
+integer :: id,i,j,k
+
+!$omp parallel do private(i,j,k)
+do id = 0, p%glb%threads-1
+    
+    do k = p%of(id)%loc%ks, p%of(id)%loc%ke
+    do j = p%of(id)%loc%js, p%of(id)%loc%je
+    do i = p%of(id)%loc%is, p%of(id)%loc%ie 
+        p%of(id)%loc%p%now(i,j,k) = p%of(id)%loc%mg(1)%sol(i-p%of(id)%loc%is+1,&
+                                                        &  j-p%of(id)%loc%js+1,&
+                                                        &  k-p%of(id)%loc%ks+1)
+                                                            
+    enddo
+    enddo
+    enddo
+    
+    call p%of(id)%bc(0,p%of(id)%loc%p%now)
+
+enddo    
+!$omp end parallel do
+
+call pt%p%sync
+
 end subroutine
