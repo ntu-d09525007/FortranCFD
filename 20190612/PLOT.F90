7bb26bd592450c7c13bf30cf49d01a38859a2de2
luis <>
2022-03-02 19:15:29 +0800

    change ls

--- 20190612/PLOT.F90
+++ 20190612/PLOT.F90
@@ -1,7 +1,7 @@
 SUBROUTINE PLOT()
 USE PRECISION
 USE PROBLEM_DEF
-USE FLUID_PROPERTIES, ONLY : U,V,W,P
+USE FLUID_PROPERTIES, ONLY : U,V,W,P,RHO,AMU
 USE LS_DATA, ONLY : PHI, HEAVY, CURV
 USE VOF_DATA, ONLY : VOF
 USE DUMMY_DATA, ONLY : F,G
@@ -33,7 +33,7 @@ CHARACTER(3) :: MET,GRI,NUM
 SELECT CASE(IBM_SOLVER)
  CASE(0)
     !WRITE(99,*) 'VARIABLES =  "X" "Y" "Z" "u" "v" "w" "p" "phi" "curvature" "Q-cri" "delta-cri" '
-    WRITE(99,*) 'VARIABLES =  "X" "Y" "Z" "vof" "phi" '
+    WRITE(99,*) 'VARIABLES =  "X" "Y" "Z" "phi"'
  CASE(1)
     WRITE(99,*) 'VARIABLES =  "X" "Y" "Z" "u" "v" "w" "phi" "vof" "SOLID"'
 END SELECT
@@ -48,7 +48,7 @@ SELECT CASE(IBM_SOLVER)
   DO J = 0, NODE_Y+1
   DO I = 0, NODE_X+1
     !WRITE(99,'(15E14.4)')X(I),Y(J),Z(K),U(I,J,K),V(I,J,K),W(I,J,K),P(I,J,K),PHI(I,J,K),CURV(I,J,K),F(I,J,K),G(I,J,K)
-    WRITE(99,'(15E14.4)')X(i),Y(J),Z(k),VOF(I,J,K),phi(I,J,K)
+    WRITE(99,'(15E14.4)')X(i),Y(J),Z(k),phi(I,J,K)
   END DO
   END DO
   END DO
